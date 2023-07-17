4d63305a914f2ec7cd66043dc5af7049f12ff806
Recep Gemalmaz <76572594+recepgemalmaz@users.noreply.github.com>
2023-07-17 11:01:57 +0300

    Ilk Android Projem
    Java-Change to Photo

---
+++ FirstApp/app/src/main/java/com/recepgemalmaz/firstapp/MainActivity.java
@@ -0,0 +1,24 @@
+package com.recepgemalmaz.firstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.security.KeyStore.SecretKeyEntry;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    
+    }
+
+    public void changeImage(View view) {
+        ImageView imageView = findViewById(R.id.imageView);
+        imageView.setImageResource(R.drawable.hermione);
+    }
+}
